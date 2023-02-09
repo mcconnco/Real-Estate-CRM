3af0fc5c4e48fa471bc64cf3bfb41f2c4dfd9656
mcconnco <mcconnco@oregonstate.edu>
2023-02-08 17:12:22 -0800

    First commit for licensee page in licensees.js and adding table properties to App.cs

--- src/pages/licensees.js
+++ src/pages/licensees.js
@@ -13,6 +13,16 @@ const Licensees = () => {
 						<p>
 							Licensees page will contain a list of licensed realtors and their contact information with the ability to add/remove/edit licensees and their information.
 						</p>
+						<table>
+							<tr>
+								<th>Licensee Number</th>
+								<th>Licensee Name</th>
+							</tr>
+							<tr>
+								<td>AA1234567</td>
+								<td>Cody McConnell</td>
+							</tr>
+						</table>
 					</div>
 				</div>
 			</section>
