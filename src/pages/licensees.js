2f78a55172678bd5a9be3f8ccd9964c39c2109fe
mcconnco <mcconnco@oregonstate.edu>
2023-02-09 13:42:50 -0800

    Updated table and css file for table border

--- src/pages/licensees.js
+++ src/pages/licensees.js
@@ -13,14 +13,18 @@ const Licensees = () => {
 						<p>
 							Licensees page will contain a list of licensed realtors and their contact information with the ability to add/remove/edit licensees and their information.
 						</p>
-						<table>
+						<table class="licensee_table" id="licensee_table">
 							<tr>
-								<th>Licensee Number</th>
-								<th>Licensee Name</th>
+								<th>Licensee Number </th>
+								<th>Licensee Name </th>
 							</tr>
 							<tr>
 								<td>AA1234567</td>
 								<td>Cody McConnell</td>
+							</tr>
+							<tr>
+								<td>BB7654321</td>
+								<td>Conner Bentzley</td>
 							</tr>
 						</table>
 					</div>
