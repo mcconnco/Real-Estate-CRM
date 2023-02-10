2bbe53566157e8f5dfc07f53c39ada4e4cff6adb
ejferte <ejferte@gmail.com>
2023-02-09 19:07:04 -0800

    login func

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,8 @@ import '../App.css';
 const Header = () => {
     return (
         <div className="navBar">
-            <a href="/"><img src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210420155809/gfg-new-logo.png" /></a>
-            <a href="/clients">Clients</a>
-            <a href="/licensees">Licensees</a>
-            <a href="/properties">Properties</a>
-            <a href="/transactions">Transactions</a>
+            <a href="/dash"><img src="https://1000logos.net/wp-content/uploads/2021/07/Oregon-State-Beavers-logo-500x281.png" width="50px" /></a>
+            <a href="/">Login</a>
         </div>
     );
 };
