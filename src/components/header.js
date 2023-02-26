f900bd4ce1bf68eb73c123a6fefd2e309109be85
mutantbroom <jon.beck@live.com>
2023-02-25 19:07:29 -0800

    fixed header and improved burger styling

--- src/components/header.js
+++ src/components/header.js
@@ -17,10 +17,14 @@ const Header = () => {
         <div className="navBar">
             
             <a href = "/dash"><img src="https://1000logos.net/wp-content/uploads/2021/07/Oregon-State-Beavers-logo-500x281.png" width="50px"/></a>
-            <a href = "/account"><img className = "account-button" src="https://cdn4.iconfinder.com/data/icons/e-commerce-181/512/477_profile__avatar__man_-80.png"></img></a>
-            
+            <button className = "account-button-container">
+                <a href = "/account"><img className = "account-button" src="https://cdn4.iconfinder.com/data/icons/e-commerce-181/512/477_profile__avatar__man_-60.png"></img></a>
+            </button>
             <nav className = "navBar">
-                <button onClick={toggleMenu}>{navbarOpen ? "Close" : "Open"}</button>
+                <button onClick={toggleMenu}>
+                    <img src="https://i.ibb.co/LJTPSJt/menu-burger-2.png" alt="menu-burger-2"></img>
+                    {navbarOpen}
+                    </button>
                 <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
                     <a href = "/clients" className = "burger-option">Clients</a>
                     <a href = "/licensees" className = "burger-option">Licensees</a>
