298f66c183c14117a291fb567c3d7e3ce9e5371e
mutantbroom <jon.beck@live.com>
2023-03-08 13:01:53 -0800

    Added Account button landing page

---
+++ src/pages/accountLanding.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './pages.css';
+
+const AccountLanding = () => {
+    return(
+        <div>
+            <section>
+                <a href = "/account">Account Settings</a>
+                <h1>
+                    YTD Spending:
+                </h1>
+            </section>
+        </div>
+    );
+};
+
+export default AccountLanding
