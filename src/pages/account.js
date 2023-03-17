eb20c4899a742f6ff7802d4c2b85d8f8bdf5a15c
mutantbroom <jon.beck@live.com>
2023-03-17 09:37:10 -0700

    Updated profile page to fully use "getUser" call

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import './pages.css';
-const { myHttpGet, myHttpGetVal } = require('../service/httpService');
-const { myHttpPost } = require('../service/httpService');
+const { myHttpGetVal } = require('../service/httpService');
+
 
 const Account = () => {
     function getUserData(myIdAgent){
@@ -10,17 +10,42 @@ const Account = () => {
 		}
         myHttpGetVal('User/getUser', params).then(data => {
 			console.log("User data: " + JSON.stringify(data));
-			fillTable(data);
+			fillAccountPage(data);
 		}).catch(error => {
 			alert("An error has ocurred: " + error);
 		});
     }
 
-    function fillTable(data) {
-        /*console.log("First Name: " + data.first_name)
+    function fillAccountPage(data) {
+        console.log("First Name: " + data.user.first_name)
         var t = ""
-        t += data.first_name
-        document.getElementById("first_name").innerHTML += t*/
+        t = data.user.first_name
+        document.getElementById("first_name").innerHTML += t
+        t = data.user.last_name
+        document.getElementById("last_name").innerHTML += t
+        t = data.user.email
+        document.getElementById("email").innerHTML += t
+        document.getElementById("public_email").innerHTML += t        
+        t = data.user.phone_num
+        document.getElementById("phone_number").innerHTML += t
+        t = data.user.username
+        document.getElementById("username").innerHTML += t
+        t = data.user.datetime_create
+        document.getElementById("birthday").innerHTML += t
+
+        t=data.user.sw_active
+        if(t===1){
+            document.getElementById("active").innerHTML += "Active"
+        }else{
+            document.getElementById("active").innerHTML += "Inactive"
+        }
+        t=data.user.sw_admin
+        if(t===1){
+            document.getElementById("admin_status").innerHTML += "Admin"
+        }else{
+            document.getElementById("admin_status").innerHTML += "Agent" 
+        }
+
     }
     useEffect(()=>{
         var currentAgent = JSON.parse(atob(localStorage.getItem("user_details")))
@@ -35,33 +60,32 @@ const Account = () => {
 						<h1>
 							Account Page
 						</h1>
-						<p>
-							This page will have a set of account settings for the user to modify.
-						</p>
+						<p id="admin_status">Account Permissions: </p>
                         
                         <h1>
                             Account Info
                         </h1>
                         <ul>
-                            <li id="email">Email: ****@gmail.com</li>
-                            <li>Username: ****</li>
-                            <li>Password: ****</li>
                             <li id="first_name">Agent Name: </li>
-                            <li id="last_name">Last Name: B****</li>
+                            <li id="last_name">Last Name: </li>
+                            <li id="email">Email: </li>
+                            <li id="username">Username: </li>
+                            <li>Password: ****</li>
+                            <li id="active">Account Status: </li>
                         </ul>
                         
                         <h1>
                             Public Methods of Contact
                         </h1>
                         <ul>
-                            <li id="email">Email: ****@gmail.com</li>
-                            <li id="phone_number">Phone Number: 555-***-****</li>
+                            <li id="public_email">Public Email: </li>
+                            <li id="phone_number">Phone Number: </li>
                             <li>Office Address: ****</li>
                         </ul>
                         <h1>
                             Account Birthday:
                         </h1>
-                        <p id="birthday">June 27th</p>
+                        <p id="birthday"></p>
 					</div>
 				</div>
 			</section>
