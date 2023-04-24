ce2dc923c2549a6ef509c64606c5bc51785be987
mutantbroom <jon.beck@live.com>
2023-04-24 16:52:43 -0700

    Added Client Name Search (Alert)

--- src/pages/clients.js
+++ src/pages/clients.js
@@ -4,7 +4,7 @@ const { myHttpGetVal } = require('../service/httpService');
 const { myHttpPost } = require('../service/httpService');
 
 const Clients = () => {
-
+	var currentUser = JSON.parse(atob(localStorage.getItem("user_details")));
 	function createClient() {
 		var first_name = document.getElementById('first_name').value;
 		var last_name = document.getElementById('last_name').value;
@@ -71,7 +71,47 @@ const Clients = () => {
 			t += tr;
 		}
 		document.getElementById("client_table").innerHTML += t;
+	}
 
+	function getClient(data, agentName){
+		var response = "";
+		var found = false;
+		for(var i = 0; i<data.agentClients.length; i++){
+			if(agentName === data.agentClients[i].first_name){
+				response += "Id: " + data.agentClients[i].id_client + "\n";
+				response += "First Name: " + data.agentClients[i].first_name + "\n";
+				response += "Last Name: " + data.agentClients[i].last_name + "\n";
+				response += "Address: " + data.agentClients[i].address + "\n";
+				response += "City: " + data.agentClients[i].city + "\n";
+				response += "Email: " + data.agentClients[i].email + "\n";
+				response += "Phone: " + data.agentClients[i].phone_num + "\n";
+				response += "Last Contact: " + data.agentClients[i].last_contact;
+				alert(response);
+				found = true;
+			}
+
+		}
+		if(!found){
+			alert("Client not found.")
+		}
+	}
+
+	/*Right now this function uses the same Client/getAllClients call as the table filling uses, it searches using the name with getClient()*/
+	function searchClient () {
+		var agentName = document.getElementById('id_client_input').value;
+		var params = {
+			"id_agent": currentUser.id_agent
+		}
+
+		myHttpGetVal('Client/getAllClients', params).then(data => {
+			if (data.success){
+				getClient(data, agentName);
+			}else {
+				alert("Clients not found!");
+			}
+		}).catch(error => {
+			alert("An error has ocurred: " + error);
+		});
 	}
 	useEffect(() => {
 		var currentAgent = JSON.parse(atob(localStorage.getItem("user_details")))
@@ -107,6 +147,11 @@ const Clients = () => {
 						<button onClick={createClient}>
 							Create Client
 						</button>
+						<div>
+							<label for="fname">Search Client:</label>
+							<input type="text" id="id_client_input" placeholder="Client Name"></input>
+							<button onClick={searchClient}>Search</button>
+						</div>
 						<table class="client_table" id="client_table" style={{ marginTop: '20px' }}>
 							<tr>
 								<th>Client ID</th>
